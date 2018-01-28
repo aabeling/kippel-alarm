60c4f50b61b41ae8a00a446b5439b92df6a8d1b8
Achim Abeling <achimabeling@web.de>
2018-01-28 16:41:47 +0100

    added notification by media

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,13 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        var self = this;
+        document.getElementById("soundtest").addEventListener("click", function() {
+
+            document.getElementById('alarm').innerHTML = "start playing sound";
+            self.playSound();
+        });
     },
 
     // Update DOM on a Received Event
@@ -39,18 +46,81 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-
         /* from https://cordova.apache.org/docs/en/latest/reference/cordova-plugin-device-motion/index.html */
-        var options = { frequency: 500 }; // Update every 3 seconds
+        var options = { frequency: 2000 };
         var watchID = navigator.accelerometer.watchAcceleration(this.onSuccess, this.onError, options);
     },
 
     onSuccess: function(acceleration) {
 
-        document.getElementById('accelerationX').innerHTML = acceleration.x;
+        document.getElementById('accelerationX').innerHTML = Math.abs(acceleration.x);
         document.getElementById('accelerationY').innerHTML = acceleration.y;
         document.getElementById('accelerationZ').innerHTML = acceleration.z;
+
+        var threshold = 0.3;
+        var thresholdExceeded = false;
+
+        if (Math.abs(acceleration.x) > threshold) {
+            document.getElementById('accelerationX').setAttribute('class', 'thresholdExceeded');
+            thresholdExceeded = true;
+        } else {
+            document.getElementById('accelerationX').setAttribute('class', 'thresholdOk');
+        }
+
+        if (Math.abs(acceleration.y) > threshold) {
+            document.getElementById('accelerationY').setAttribute('class', 'thresholdExceeded');
+            thresholdExceeded = true;
+        } else {
+            document.getElementById('accelerationY').setAttribute('class', 'thresholdOk');
+        }
+
+        if (Math.abs(acceleration.z) > threshold) {
+            document.getElementById('accelerationZ').setAttribute('class', 'thresholdExceeded');
+        } else {
+            document.getElementById('accelerationZ').setAttribute('class', 'thresholdOk');
+        }
+
+
+        if (thresholdExceeded) {
+
+            document.getElementById('alarm').setAttribute('class', 'thresholdExceeded');
+            app.playSound();
+
+        } else {
+            document.getElementById('alarm').setAttribute('class', 'thresholdOk');
+        }
+
+    },
+
+    playSound: function() {
+
+        document.getElementById('alarm').innerHTML = "play sound";
+
+        var media = new Media(this.getMediaURL("sounds/hey.mp3"),
+            this.mediaSuccess,
+            this.mediaError,
+            this.mediaStatus);
+        media.play();
+    },
+
+    getMediaURL: function(s) {
+        if (cordova.platformId.toLowerCase() === "android") return "/android_asset/www/" + s;
+        return s;
+    },
+
+    mediaSuccess: function() {
+
+        document.getElementById('alarm').innerHTML = "media played";
+    },
+
+    mediaError: function(e) {
+
+        document.getElementById('alarm').innerHTML = JSON.stringify(e);
+    },
+
+    mediaStatus: function(status) {
+
+        document.getElementById('alarm').innerHTML = "status: " + status;
     },
 
     onError: function() {
